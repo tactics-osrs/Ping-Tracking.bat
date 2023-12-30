882cdfa1ebad63ec34d0cc7b624b79a3869d0156
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2023-12-30 17:10:49 -0500

    Update Ping Tracking.bat

--- Ping Tracking.bat
+++ Ping Tracking.bat
@@ -1,9 +1,16 @@
-TITLE Ping Tracking
 @echo off
+setlocal enabledelayedexpansion
+:input
 set /p website="Enter the domain you want to ping (e.g., github.com): "
 :loop
 echo %time% >> pingresults.txt
-ping %website% -n 1 >> pingresults.txt
+ping %website% -n 1 | find "Reply from" >nul
+if errorlevel 1 (
+    echo No response from %website%, please check the domain and try again.
+    goto input
+) else (
+    ping %website% -n 1 >> pingresults.txt
+)
 echo Tracking ping again in:
 timeout /t 5
 goto loop
